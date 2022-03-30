18c40765642d6f2b6397ec158b9b25f495e43664
Mayestique <100190895+Mayestique@users.noreply.github.com>
2022-03-30 12:08:50 +0200

    Add files via upload

---
+++ 5. Kontroler/app/controllers/CalcCtrl.class.php
@@ -0,0 +1,109 @@
+<?php
+// W skrypcie definicji kontrolera nie trzeba dołączać problematycznego skryptu config.php,
+// ponieważ będzie on użyty w miejscach, gdzie config.php zostanie już wywołany.
+
+require_once 'CalcForm.class.php';
+require_once 'CalcResult.class.php';
+
+
+class CalcCtrl {
+
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+
+	/** 
+	 * Konstruktor - inicjalizacja właściwości
+	 */
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+	}
+	
+	/** 
+	 * Pobranie parametrów
+	 */
+	public function getParams(){
+		$this->form->kwota = getFromRequest('kwota');
+		$this->form->ile = getFromRequest('ile');
+		$this->form->procent = getFromRequest('procent');
+	}
+	
+	/** 
+	 * Walidacja parametrów
+	 * @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 */
+	public function validate() {
+		// sprawdzenie, czy parametry zostały przekazane
+		if (! (isset ( $this->form->kwota ) && isset ( $this->form->ile ) && isset ( $this->form->procent ))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false; //zakończ walidację z błędem
+		}
+		
+		// sprawdzenie, czy potrzebne wartości zostały przekazane
+		if ($this->form->kwota == "") {
+			getMessages()->addError('Nie podano kwoty kredytu');
+		}
+		if ($this->form->ile == "") {
+			getMessages()->addError('Nie podano ilości miesięcy');
+		}
+		if ($this->form->procent == "") {
+			getMessages()->addError('Nie podano oprocentowania');
+		}
+		
+		// nie ma sensu walidować dalej gdy brak parametrów
+		if (! getMessages()->isError()) {
+			
+			// sprawdzenie, czy $x i $y są liczbami całkowitymi
+			if ((! is_numeric( $this->form->kwota )) || (! is_numeric( $this->form->ile )) || (! is_numeric( $this->form->procent ))) {
+				getMessages()->addError('TO NIE JEST LICZBA!');
+			}
+			
+		}
+		
+		return ! getMessages()->isError();
+	}
+	
+	/** 
+	 * Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 */
+	public function process(){
+
+		$this->getparams();
+		
+		if ($this->validate()) {
+				
+			//konwersja parametrów na int
+			$this->form->kwota = intval($this->form->kwota);
+			$this->form->ile = intval($this->form->ile);
+			$this->form->procent = intval($this->form->procent);
+			getMessages()->addInfo('Parametry poprawne.');
+			
+			$this->result->result = round(((($this->form->kwota * ($this->form->procent*0.01))+$this->form->kwota)/$this->form->ile),2);
+			
+			getMessages()->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	
+	
+	/**
+	 * Wygenerowanie widoku
+	 */
+	
+	public function generateView(){
+		//nie trzeba już tworzyć Smarty i przekazywać mu konfiguracji i messages
+		// - wszystko załatwia funkcja getSmarty()
+		
+		getSmarty()->assign('page_title','Kalkulator kredytowy');
+		getSmarty()->assign('page_description','Wylicz w łatwy sposób swoją ratę');
+		getSmarty()->assign('page_header','Kalkulator kredytowy');
+		getSmarty()->assign('page_footer','autor szablonu: Marta Skowronek PAW3');
+					
+		getSmarty()->assign('form',$this->form);
+		getSmarty()->assign('res',$this->result);
+		
+		getSmarty()->display('CalcView.html'); // już nie podajemy pełnej ścieżki - foldery widoków są zdefiniowane przy ładowaniu Smarty
+	}
+}
